5b588d55734e41ada2d06c109f170941d407a440
Ganesh J <52096566+Abiganesh@users.noreply.github.com>
2021-08-22 21:56:34 +0530

    Add files via upload

---
+++ which divison.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(0,n):
+    rating=int(input())
+    if(rating>=2000):
+        print(1)
+    elif(rating>=1600) and (rating<2000):
+        print(2)
+    else:
+        print(3)
