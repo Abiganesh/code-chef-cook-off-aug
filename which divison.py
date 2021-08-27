ad43d78bf93ead358ec6739fbd1276ecc73c3d86
Ganesh J <52096566+Abiganesh@users.noreply.github.com>
2021-08-27 22:42:48 +0530

    Update which divison.py

--- which divison.py
+++ which divison.py
@@ -3,7 +3,7 @@ for i in range(0,n):
     rating=int(input())
     if(rating>=2000):
         print(1)
-    elif(rating>=1600) and (rating<2000):
+    elif(rating >= 1600) and (rating < 2000):
         print(2)
     else:
         print(3)
